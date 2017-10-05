46474fac7364bbe618dbf416f421369fdeb8f873
carolina-silvag <carolinasguzman@gmail.com>
2017-10-05 01:40:41 -0300

    tarjeta valida con README y comentarios

---
+++ README.md
@@ -0,0 +1,22 @@
+pasos para tarjeta de credito valida
+
+Crear una variable para ingresar numero de tarjeta, utilizar un prompt (solo deben ser numeros)
+Crear un funcion llamada validateNmuber, para validar el numero ingresado y dejar los numero validos en un array, para ser utilizados con mas comodidad.
+Crear una variable vacia del tipo array, donde estaran los numeros validos.
+Crear una variable que contenga los numero ingresado en orden desde atras hacia adelante.
+Crear una variable con todos los numero validos.
+Luego preguntar con un if si es vacio, de no ser vacio, preguntar si cada numero se encuentras en numeros validos, si se cumple una de ambos if, volver a preguntar para que ingrese numeros validos.
+De lo contrario que guarde los numeros en la variable vacia array.
+Por ultimo retornar los numeros validos en un array.
+
+Crear un function isValidCard que me validara la tarjeta, entregando si es una tarjeta valida o no es valida, de no ser valida debe volver a preguntar para ingresar numeros validos de una tarjeta.
+Crear una variable vacia donde se sumara todos los numeros validos.
+Luego con un for recorrer todos los numeros del array, y con un if preguntar si es una posicion par tomando desde 1 a 9, por lo cual, 0 en realidad era uno, 1 en realidad era 2 y asi sucesivamente hasta 10, ademas tenia que cumplir con la condicion de que si ese numero multiplicado por 2 era mayor a 9, entonces debia separarlo (tranformarlo en string para tener el numero en posicion cero y uno) y luego volver a tranformar en un number con parseInt y sumarlo a la variable vacia.(mejor explicado con el logaritmo de luhn).
+Si era menor a 9 y posicion par entonces que lo multiplicar por 2 y lo sumara a la variable vacia. 
+De lo contrario solo sumar el numero a la variable vacia
+Por ultimo, preguntar si el resto de la suma dividido por 10 es cero entonces rectornar 'tarjeta valida', de lo contrario retornar 'tarjeta invalida' y volver a preguntar para ingresar un numero valido.
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 var userTarjeta= prompt('Ingrese numeros de tarjeta');
-isValidCard(validateNmuber(userTarjeta))
+isValidCard(validateNumber(userTarjeta))
 
 /* he creado la funcion validateNumber para ver si el numero ingresedo es solamente un numero,
 de lo contrario que vuelva a decir que se ingrese numeros valido*/
-function validateNmuber(userTarjeta){
+function validateNumber(userTarjeta){
 	var valit = [];
-	var numberTarjeta = userTarjeta.split('').reverse().join('');
+	var numberTarjeta = userTarjeta.split('').reverse().join(''); // convertir los numero ingresados en posicion de atras hacia adelante
 	var number = '0123456789';
-	if(numberTarjeta.length == 0){
-		return validateNmuber(prompt('Ingrese numeros validos'));
+	if(numberTarjeta.length == 0){              //comprobar si es vacio, si cumple la condicion de ser igual a cero
+		return validateNumber(prompt('Ingrese numeros validos')); //volver a preguntar para que se ingrese un numeros validos
 	}
-	for(var i=0; i< numberTarjeta.length; i++){
-		if(number.indexOf(numberTarjeta[i]) == -1){
-				return validateNmuber(prompt('Ingrese numeros validos'));
+	for(var i=0; i< numberTarjeta.length; i++){     //recorrer todos lo numeros
+		if(number.indexOf(numberTarjeta[i]) == -1){ //si el numero no se encuentra en la variable number sera igual a menos uno
+				return validateNumber(prompt('Ingrese numeros validos'));//vuelve a preguntar para que ingrese numeros validos
 		}else{
-			valit.push(numberTarjeta[i])
+			valit.push(numberTarjeta[i])  //agrega a valit los numeros validos
 		}	
 	}
 	return valit;
 }
 
-/* funcion pedida en ejercicio 2 para ver si la tarjeta es valida*/
+/* funcion pedida en ejercicio 2 para ver si la tarjeta es valida, utilizar logaritmo
+luhn */
 function isValidCard(number){
 	var sum = 0;
-	for(var i=0; i<number.length; i++){
+	for(var i=0; i<number.length; i++){               
 		if((i+1)%2 ==0 && parseInt(number[i])*2 >9){ //para sumar numeros que sean mayores a 9 debes cunplir las condiciones
-			var str = (parseInt(number[i])*2)+'' ;   //lo convertir en un number ya que era un string y lo multiplique luego sume '', para que fuera strung de nuevo
-			sum += parseInt(str[0])+parseInt(str[1]) //sume ambos numeros
+			var str = (parseInt(number[i])*2)+'' ;   //lo converti en un number, ya que era un string y lo multiplique por 2, luego sume '', para que fuera strung de nuevo
+			sum += parseInt(str[0])+parseInt(str[1]) //sume ambos numeros, utilize parseInt para tranformarlo a numero otra vez
 		}
 		else if((i+1)%2 ==0){            //ya que por condicion anterior no son mayores a 9 entonces todos los que estan en posicion par son multiplicado
 			sum += parseInt(number[i])*2
@@ -40,6 +41,6 @@ function isValidCard(number){
 	}
 	else{                        //de lo contrario es una tarjeta invalida y debe volver a preguntar
 		alert('tarjeta invalida')
-		return isValidCard(validateNmuber(prompt('Ingrese numeros validos')));
+		return isValidCard(validateNumber(prompt('Ingrese numeros validos')));
 	}
 }
